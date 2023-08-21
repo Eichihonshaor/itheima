c77d78819cc636902bf9ee24b8387c78d0dede6d
HTD <3134442693@qq.com>
2023-08-21 15:48:14 +0800

    Initial commit

---
+++ day06/src/com/itheima/d6_map/MapTest1.java
@@ -0,0 +1,26 @@
+package com.itheima.d6_map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * 目标 : 掌握Map集合的特点
+ */
+public class MapTest1 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("手表", 100);
+        map.put("手表", 200);
+        map.put("手机", 100);
+        System.out.println(map);
+
+
+        /**
+         * Map集合的常用方法
+         * */
+
+        //获取集合大小
+        
+    }
+}
